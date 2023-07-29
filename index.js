a1c1725670e2b37d4055001e65600d41ae9835a9
Ursula4002 <135629535+Ursula4002@users.noreply.github.com>
2023-07-29 15:37:54 +0000

    Motivational Quotes

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // comment
+
+const motivationalQuote = [
+    "Keep smilling because life's a beautiful thing and ther's so much to smile 'bout",
+    "Life is either a daring adventure or nothing at all",
+    "When things go wrong don't go with them",
+    "Happiness never decreases by being shared",
+    "No one can make you feel inferior without your consent",
+    "Like is like riding a bike , to keep your balance you must moving moving"
+]
+let index = Math.floor(Math.random()*6);
+// console.log(index);
+console.log(motivationalQuote[index]);
